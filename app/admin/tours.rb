03d51984b9e75fc46b8eabfe74bf77d5beaf1c09
Raghu Betina <raghu@firstdraft.com>
2019-12-03 05:04:56 +0000

    Whitelist columns in Tour for ActiveAdmin

--- app/admin/tours.rb
+++ app/admin/tours.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Tour do
+
+ permit_params :name, :starts_on, :ends_on, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
